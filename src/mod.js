76e7940ae633200d354cb004bd67e580a547a93d
Andrei Georgescu <g.andrei.georgescu@gmail.com>
2025-11-16 22:32:56 +0200

    Fire arrow skills

--- src/mod.js
+++ src/mod.js
@@ -1,10 +1,12 @@
 const explodingArrowRangeIncrease = +config.explodingArrowRangeIncrease;
 const explodingArrowMaxRange = +config.explodingArrowMaxRange;
-const explodingArrowDgmSynergy = +config.explodingArrowDgmSynergy;
+const explodingArrowFireArrowDgmSynergy = +config.explodingArrowFireArrowDgmSynergy;
+const explodingArrowImmolationArrowDgmSynergy = +config.explodingArrowImmolationArrowDgmSynergy;
 
 const immolationArrowRangeIncrease = +config.immolationArrowRangeIncrease;
 const immolationArrowMaxRange = +config.immolationArrowMaxRange;
-const immolationArrowDgmSynergy = +config.immolationArrowDgmSynergy;
+const immolationArrowFireArrowDgmSynergy = +config.immolationArrowFireArrowDgmSynergy;
+const immolationArrowExplodingArrowDgmSynergy = +config.immolationArrowExplodingArrowDgmSynergy;
 const immolationArrowDgmPerSecondSynergy = +config.immolationArrowDgmPerSecondSynergy;
 
 // ===== skills.txt =====
@@ -15,13 +17,17 @@ skills.rows.forEach((row) => {
     if (row["skill"] === "Exploding Arrow") {
         // Explosion range
         row.calc1 = `min((skill('Fire Arrow'.blvl))*${explodingArrowRangeIncrease}+5, ${explodingArrowMaxRange})`;
-        // Explosion damage synergy
-        row.Param8 = explodingArrowDgmSynergy;
+        // Explosion damage synergies
+        row.Param8 = explodingArrowFireArrowDgmSynergy;
+        row.Param9 = explodingArrowImmolationArrowDgmSynergy;
+        row.EDmgSymPerCalc = `(skill('Fire Arrow'.blvl)) * par8 + (skill('Immolation Arrow'.blvl)) * par9`;
     } else if (row["skill"] === "Immolation Arrow") {
         // Explosion range
         row.calc2 = `min((skill('Fire Arrow'.blvl))*${immolationArrowRangeIncrease}+4, ${immolationArrowMaxRange})`;
-        // Explosion damage synergy
-        row.Param8 = immolationArrowDgmSynergy;
+        // Explosion damage synergies
+        row.Param8 = immolationArrowExplodingArrowDgmSynergy;
+        row.Param9 = immolationArrowFireArrowDgmSynergy;
+        row.EDmgSymPerCalc = `(skill('Exploding Arrow'.blvl)) * par8 + (skill('Fire Arrow'.blvl)) * par9`;
     }
 });
 
@@ -37,7 +43,7 @@ missiles.rows.forEach((row) => {
         row.sHitPar1 = 0;
     } else if (row.Missile === 'immolationfire') {
         // Damage per second
-        row.EDmgSymPerCalc = `skill('Fire Arrow'.lvl) * ${immolationArrowDgmPerSecondSynergy}`;
+        row.EDmgSymPerCalc = `skill('Fire Arrow'.blvl) * ${immolationArrowDgmPerSecondSynergy}`;
     }
 });
 
@@ -48,8 +54,21 @@ const skilldescFilename = 'global\\excel\\skilldesc.txt';
 const skilldesc = D2RMM.readTsv(skilldescFilename);
 
 skilldesc.rows.forEach((row) => {
-    if (row["skilldesc"] === "immolation arrow") {
-        row["dsc3calca2"] = immolationArrowDgmPerSecondSynergy;
+    if (row["skilldesc"] === "exploding arrow") {
+        if (explodingArrowImmolationArrowDgmSynergy) {
+            row.dsc3line3 = 76;
+            row.dsc3texta3 = 'Firedplev';
+            row.dsc3textb3 = 'skillname27';
+            row.dsc3calca3 = 'par9';
+        }
+    } else if (row["skilldesc"] === "immolation arrow") {
+        row.dsc3calca2 = immolationArrowDgmPerSecondSynergy;
+        if (immolationArrowFireArrowDgmSynergy) {
+            row.dsc3line4 = 76;
+            row.dsc3texta4 = 'Firedplev';
+            row.dsc3textb4 = 'skillname7';
+            row.dsc3calca4 = 'par9';
+        }
     }
 });
 
