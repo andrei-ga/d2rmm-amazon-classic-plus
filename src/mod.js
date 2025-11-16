c2289a905f2f3935bdbc6279bfa45368014dddd1
Andrei Georgescu <g.andrei.georgescu@gmail.com>
2025-11-17 00:34:23 +0200

    Freezing Arrow bonuses

--- src/mod.js
+++ src/mod.js
@@ -9,6 +9,11 @@ const immolationArrowFireArrowDgmSynergy = +config.immolationArrowFireArrowDgmSy
 const immolationArrowExplodingArrowDgmSynergy = +config.immolationArrowExplodingArrowDgmSynergy;
 const immolationArrowDgmPerSecondSynergy = +config.immolationArrowDgmPerSecondSynergy;
 
+const freezingArrowRangeIncrease = +config.freezingArrowRangeIncrease;
+const freezingArrowMaxRange = +config.freezingArrowMaxRange;
+const freezingArrowColdArrowDmgSynergy = +config.freezingArrowColdArrowDmgSynergy;
+const freezingArrowIceArrowDmgSynergy = +config.freezingArrowIceArrowDmgSynergy;
+
 // ===== skills.txt =====
 const skillsFilename = 'global\\excel\\skills.txt';
 const skills = D2RMM.readTsv(skillsFilename);
@@ -16,18 +21,31 @@ const skills = D2RMM.readTsv(skillsFilename);
 skills.rows.forEach((row) => {
     if (row["skill"] === "Exploding Arrow") {
         // Explosion range
-        row.calc1 = `min((skill('Fire Arrow'.blvl))*${explodingArrowRangeIncrease}+5, ${explodingArrowMaxRange})`;
+        if (explodingArrowRangeIncrease) {
+            row.calc1 = `min((skill('Magic Arrow'.blvl))*${explodingArrowRangeIncrease}+3, ${explodingArrowMaxRange})`;
+        }
         // Explosion damage synergies
         row.Param8 = explodingArrowFireArrowDgmSynergy;
         row.Param9 = explodingArrowImmolationArrowDgmSynergy;
         row.EDmgSymPerCalc = `(skill('Fire Arrow'.blvl)) * par8 + (skill('Immolation Arrow'.blvl)) * par9`;
     } else if (row["skill"] === "Immolation Arrow") {
         // Explosion range
-        row.calc2 = `min((skill('Fire Arrow'.blvl))*${immolationArrowRangeIncrease}+4, ${immolationArrowMaxRange})`;
+        if (immolationArrowRangeIncrease) {
+            row.calc2 = `min((skill('Magic Arrow'.blvl))*${immolationArrowRangeIncrease}+3, ${immolationArrowMaxRange})`;
+        }
         // Explosion damage synergies
         row.Param8 = immolationArrowExplodingArrowDgmSynergy;
         row.Param9 = immolationArrowFireArrowDgmSynergy;
         row.EDmgSymPerCalc = `(skill('Exploding Arrow'.blvl)) * par8 + (skill('Fire Arrow'.blvl)) * par9`;
+    } else if (row["skill"] === "Freezing Arrow") {
+        // Explosion range
+        if (freezingArrowRangeIncrease) {
+            row.calc1 = `min((skill('Magic Arrow'.blvl))*${freezingArrowRangeIncrease}+3, ${freezingArrowMaxRange})`;
+        }
+        // Explosion damage synergies
+        row.Param8 = freezingArrowColdArrowDmgSynergy;
+        row.Param9 = freezingArrowIceArrowDmgSynergy;
+        row.EDmgSymPerCalc = `(skill('Cold Arrow'.blvl)) * par8 + (skill('Ice Arrow'.blvl)) * par9`;
     }
 });
 
@@ -44,6 +62,9 @@ missiles.rows.forEach((row) => {
     } else if (row.Missile === 'immolationfire') {
         // Damage per second
         row.EDmgSymPerCalc = `skill('Fire Arrow'.blvl) * ${immolationArrowDgmPerSecondSynergy}`;
+    } else if (row.Missile === 'freezingarrowexp3') {
+        // Explosion Radius (If 0, then use Skill calc1)
+        row.sHitPar1 = 0;
     }
 });
 
@@ -61,6 +82,16 @@ skilldesc.rows.forEach((row) => {
             row.dsc3textb3 = 'skillname27';
             row.dsc3calca3 = 'par9';
         }
+        if (explodingArrowRangeIncrease) {
+            row.dsc2line1 = 36;
+            row.dsc2texta1 = 'StrSkillRadiusSingular';
+            row.dsc2textb1 = '';
+            row.dsc2calca1 = `min((skill('Magic Arrow'.blvl))*${explodingArrowRangeIncrease}+3, ${explodingArrowMaxRange})`;
+            row.dsc2calcb1 = '';
+
+            row.dsc3line4 = 18;
+            row.dsc3texta4 = 'skillname6';
+        }
     } else if (row["skilldesc"] === "immolation arrow") {
         row.dsc3calca2 = immolationArrowDgmPerSecondSynergy;
         if (immolationArrowFireArrowDgmSynergy) {
@@ -68,6 +99,30 @@ skilldesc.rows.forEach((row) => {
             row.dsc3texta4 = 'Firedplev';
             row.dsc3textb4 = 'skillname7';
             row.dsc3calca4 = 'par9';
+        }
+        if (immolationArrowRangeIncrease) {
+            row.dsc2line2 = 36;
+            row.dsc2texta2 = 'StrSkillRadiusSingular';
+            row.dsc2textb2 = '';
+            row.dsc2calca2 = `min((skill('Magic Arrow'.blvl))*${immolationArrowRangeIncrease}+3, ${immolationArrowMaxRange})`;
+            row.dsc2calcb2 = '';
+
+            row.dsc3line5 = 18;
+            row.dsc3texta5 = 'skillname6';
+        }
+    } else if (row["skilldesc"] === "freezing arrow") {
+        if (freezingArrowIceArrowDmgSynergy) {
+            row.dsc3line4 = 76;
+            row.dsc3texta4 = 'Colddplev';
+            row.dsc3textb4 = 'skillname21';
+            row.dsc3calca4 = 'par9';
+        }
+        if (freezingArrowRangeIncrease) {
+            row.dsc2calca1 = `min((skill('Magic Arrow'.blvl))*${freezingArrowRangeIncrease}+3, ${freezingArrowMaxRange})`;
+            row.dsc2calcb1 = '';
+
+            row.dsc3line5 = 18;
+            row.dsc3texta5 = 'skillname6';
         }
     }
 });
